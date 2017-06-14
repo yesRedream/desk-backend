54914c73583a0af3482eb9f55de58787cf417c60
Andrii Pohrebniak <andrii.pohrebniak@gmail.com>
2017-06-14 19:50:03 +0300

    thymeleaf

--- src/main/java/com/apo/config/AppConfig.java
+++ src/main/java/com/apo/config/AppConfig.java
@@ -5,12 +5,18 @@ import com.apo.model.Desk;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
 /**
  * Created by Andrii Pohrebniak andrii.pohrebniak@gmail.com on 05/06/2017.
  */
 @Configuration
-public class AppConfig {
+@EnableWebMvc
+public class AppConfig extends WebMvcConfigurerAdapter{
     @Autowired
     private MongoService mongoService;
 
---
+++ src/main/java/com/apo/controllers/ViewController.java
@@ -0,0 +1,20 @@
+package com.apo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.View;
+
+/**
+ * Created by Andrii Pohrebniak andrii.pohrebniak@gmail.com on 14/06/2017.
+ */
+@Controller
+@RequestMapping("/")
+public class ViewController {
+
+    @GetMapping("/hello")
+    public String home(){
+        return "home";
+    }
+
+}
